8b7a263a4330463870bf469e5c63bf2c3d326089
Navya Madhuri Mekala CW <ahlb5zz@mmm.com>
2026-03-24 12:33:54 +0530

    first code

---
+++ tests/demo/mytest.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test("testing the page title", async ({ page }) => {
+  await page.goto("https://katalon-demo-cura.herokuapp.com/");
+
+  await expect(page).toHaveTitle("CURA Healthcare Service");
+  await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+});
